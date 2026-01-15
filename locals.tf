547263aac4bdc5052d0d59bfb03069ff932cc016
Dan Ball <dan@goodparty.org>
2026-01-15 16:27:47 -0500

    Remove extra tfvars

--- locals.tf
+++ locals.tf
@@ -1,8 +1,6 @@
 locals {
-  # Load mart configuration from YAML
   marts_config = yamldecode(file("${path.module}/config/marts.yaml"))
   marts        = local.marts_config.marts
 
-  # Create map for easier reference
   marts_map = { for mart in local.marts : mart.name => mart }
 }
